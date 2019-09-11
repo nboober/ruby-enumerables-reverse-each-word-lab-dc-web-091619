10c8f1d3c82cbd272b788a00c3661e02fdbbf4a8
Nicholas Boober <nickboober92@gmail.com>
2019-09-11 15:45:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,10 @@ def reverse_each_word(string)
   
   split_string = string.split(" ")
   
-  # reversed = string.collect{|item| item.reverse!}
+  reversed = string.collect{|item| item.reverse!}
   
-  # newstring = reversed.join()
+  newstring = reversed.join()
   
-  return split_string
+  return newstring
   
 end
