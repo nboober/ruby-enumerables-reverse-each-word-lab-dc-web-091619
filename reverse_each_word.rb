9a03bc1ef0811051111eb92128e74e61e2c0a539
Nicholas Boober <nickboober92@gmail.com>
2019-09-11 15:46:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word(string)
   
   reversed = string.collect{|item| item.reverse!}
   
-  newstring = reversed.join()
+  # newstring = reversed.join()
   
-  return newstring
+  return reversed
   
 end
